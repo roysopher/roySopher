bb28f21cf4ecd14f359e01cbe94b3d82120f4a21
Roy Sopher <sopherroy@gmail.com>
2021-12-19 19:42:26 +0200

    7&8

---
+++ Ass_7+8/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, redirect, url_for
+from flask import render_template
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=8000)
+
+@app.route('/')
+def home_func():  # put application's code here
+    return render_template('info.html', name='Roy')
+
+@app.route('/info', methods=['GET', 'POST'])
+def about_func():  # put application's code here
+    name = 'Roy'
+    second_name = 'Sopher'
+    uni = 'BGU'
+    return render_template('profile.html',
+                           profile={'Name': ' Roy', 'Family Name' : ' Sopher'},
+                           name = name,
+                           university=uni,
+                           second_name=second_name,
+                           degreas=['BSc', 'MSc'],
+                           hobies=('Football', 'Music', 'Chess'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
